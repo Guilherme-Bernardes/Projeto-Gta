64ba818a63b058cd6944d8217144cae442970d9f
Guilherme Bernardes Cruz <145566353+Guilherme-Bernardes@users.noreply.github.com>
2024-05-03 10:47:12 -0300

    .

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
 const musicaGta=new Audio("/songs/musicagta.mp3")
 
-musicaGta.play()
-musicaGta.volume=0.40
-musicaGta.loop=true
+musicaGta.play();
+musicaGta.volume=0.40;
+musicaGta.loop=true;
 const butao =document.querySelector(".butao-plataforma");
 const elementoPlataformas=document.querySelector(".butao-plataforma .plataformas" );
 butao.addEventListener("click",()=>{
